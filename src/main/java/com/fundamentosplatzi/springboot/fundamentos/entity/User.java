74aef0ccaf7a471b019a99f90fbac0b20a56ff9a
Sebastian Tarifa <starifa@exisoft.com.ar>
2023-05-03 16:19:04 -0300

    Metodos create update y delete del crud

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/User.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/User.java
@@ -34,6 +34,10 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    public User(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/service/UserService.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/service/UserService.java
@@ -29,4 +29,24 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User save(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    public void delete(Long id) {
+        userRepository.delete(new User(id));
+    }
+
+    public User update(User newUser, Long id) {
+        return userRepository.findById(id)
+                .map(
+                        user -> {
+                            user.setEmail(newUser.getEmail());
+                            user.setBirthDate(newUser.getBirthDate());
+                            user.setName(newUser.getName());
+                            return userRepository.save(user);
+                        }
+                ).get();
+
+    }
 }
